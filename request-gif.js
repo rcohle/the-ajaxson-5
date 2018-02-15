c4208109a7d1ef83702439a8bc8c1335de3ab278
Rosanna Cohle <rosanna76@gmail.com>
2018-02-15 02:07:36 -0600

    add css

--- request-gif.js
+++ request-gif.js
@@ -2,72 +2,53 @@ $(document).ready(function() {
     // register our function as the "callback" to be triggered by the form's submission event
     $("#form-gif-request").submit(fetchAndDisplayGif); // in other words, when the form is submitted, fetchAndDisplayGif() will be executed
 });
-
-
-
-
 /**
  * sends an asynchronous request to Giphy.com aksing for a random GIF using the 
  * user's search term (along with "jackson 5")
  * 
  * upon receiving a response from Giphy, updates the DOM to display the new GIF
  */
-function fetchAndDisplayGif(event) {
-    
+function fetchAndDisplayGif(event) { 
     // This prevents the form submission from doing what it normally does: send a request (which would cause our page to refresh).
     // Because we will be making our own AJAX request, we dont need to send a normal request and we definitely don't want the page to refresh.
     event.preventDefault();
-
     if ($("#test").val() == 5 || $("#test").val().toLowerCase() == "five") {
-    
     // get the user's input text from the DOM
     var searchQuery = $("input[name=tag]").val();
     tagString = "Jackson 5" + " " + searchQuery
      // TODO should be e.g. "dance"
-
     // configure a few parameters to attach to our request
     var params = { 
         api_key: "dc6zaTOxFJmzC", 
         tag : tagString // TODO should be e.g. "jackson 5 dance"
     };
-    
     // make an ajax request for a random GIF
     $.ajax({
         url: "https://api.giphy.com/v1/gifs/random", // TODO where should this request be sent?
         data: params, // attach those extra parameters onto the request
         success: function(response) {
-            // if the response comes back successfully, the code in here will execute.
-            
+            // if the response comes back successfully, the code in here will execute.      
             // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
             console.log("we received a response!");
             console.log(response);
-
             $("#gif").attr("src", response.data.image_url);
             setGifLoadedStatus(true);
-
         },
             // TODO
             // 1. set the source attribute of our image to the image_url of the GIF
             // 2. hide the feedback message and display the image
-        
-
         error: function () {
-
             $("#feedback").text("Blame it On the Boogie... we could not complete your request... please try again.");
             setGifLoadedStatus(false);   
         }
     });
-        
         $("#feedback").text("Please don't go, girl... coming up!");
-
 }
  else {
-   
     $("#feedback").text("You and I must make a pact - to answer questions correctly. Sorry, try again.");
     setGifLoadedStatus(false);
 }        
 }
-    
 /**
  * toggles the visibility of UI elements based on whether a GIF is currently loaded.
  * if the GIF is loaded: displays the image and hides the feedback label
